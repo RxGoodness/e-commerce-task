50670bcc9c7816603f1b4cb3a69e4c4676b1a505
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-15 00:54:47 +0100

    refactoring

--- middleware/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './auth';
-export * from './validator';
-export * from './error';
-export * from './errorHandler';
-export * from './notFound';
-export * from './pagination';
-
--- utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './bycrypt';
-export * from './gen.token';
