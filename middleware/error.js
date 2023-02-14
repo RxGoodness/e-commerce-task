ba44c271aebce5c43f735ffee577fe6885251db8
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-14 20:52:13 +0100

    fixes

--- middleware/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorsHandler = void 0;
-const errorsHandler = (err, req, res, next) => {
-    console.error(err.message);
-    return res.status(500).send('Server Error');
-};
-exports.errorsHandler = errorsHandler;
