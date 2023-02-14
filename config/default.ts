438ae68f3eb14489a90c744a13245309ff03b661
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-14 18:44:00 +0100

    adding database configuration and common environment variables

--- config/default.ts
+++ config/default.ts
@@ -4,6 +4,6 @@ dotenv.config();
 
 export default {
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || "secret",
-  mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/ecommerce",
+  jwtSecret: process.env.JWT_SECRET as string,
+  mongoURI: process.env.MONGO_URI 
 };
