7d004c54a5110dff3e373b74e190f998f3de8468
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-14 22:06:18 +0100

    fixes

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
-import app from './app';
 import db  from './config/db';
 import envValues  from './config/default';
 import dotenv from "dotenv";
-
+// import app from './app';
 dotenv.config();
+import app from './app';
 
 const startServer = async () => {
     try {
